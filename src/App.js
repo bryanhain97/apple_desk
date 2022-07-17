92318add928c9d9f747e799c8a9c68a9aadbfceb
Bryan Hain <bryanhain97@gmail.com>
2022-07-17 21:13:23 +0200

    update click event

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ImFinder } from 'react-icons/im'
 function App() {
   const [open, setOpen] = useState(true)
   const buttonRef = useRef(null)
+  const buttonText = useRef(null)
   const finderIcon = document.querySelector('.button_tab_icon')
 
   const variants = {
@@ -14,13 +15,21 @@ function App() {
     tabClosed: { opacity: [1, 1, 1, 1, 1, 0], scale: 1 }
   }
   const toggleProgram = () => {
+    if (finderIcon.classList.contains('clicked') && buttonText.classList.contains('clicked')) {
+      finderIcon.classList.remove('clicked')
+      buttonText.classList.remove('clicked')
+    }
     buttonRef.current.style.cursor = 'progress'
-    finderIcon?.classList.remove('clicked')
     setTimeout(() => {
       setOpen(true)
       buttonRef.current.style.cursor = 'default'
     }, 160)
   }
+  const toggleClicked = () => {
+    finderIcon?.classList.toggle('clicked')
+    buttonText.current.classList.toggle('clicked')
+  }
+
 
   return (
     <div className="App">
@@ -31,9 +40,9 @@ function App() {
       >
         <Tab setOpen={setOpen} open={open} />
       </motion.div>
-      <button className='button_tab' ref={buttonRef} onClick={() => finderIcon?.classList.add('clicked')} onDoubleClick={toggleProgram}>
+      <button className='button_tab' ref={buttonRef} onClick={toggleClicked} onDoubleClick={toggleProgram}>
         <ImFinder className='button_tab_icon' />
-        <span className='button_tab_name'>Finder</span>
+        <span className='button_tab_text' ref={buttonText}>Finder</span>
       </button>
     </div >
   );
