61d61da9bf2922a77858aa9f98a178de5b57e739
Bryan Hain <bryanhain97@gmail.com>
2022-07-31 17:49:10 +0200

    add some projects

--- src/components/content/Schreibtisch.jsx
+++ src/components/content/Schreibtisch.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Project from './helper/Project'
 import rtfFish from '../../img/rtf_fish.png'
+import wireframe from '../../img/wireframe.png'
+import portfolio from '../../img/portfolio.png'
 
 const Content = () => {
     return (
@@ -10,9 +12,29 @@ const Content = () => {
                 <Project
                     imageSrc={rtfFish}
                     alt='react three fiber fish'
-                    title='R3F Fish'
+                    title='3D Fish'
+                    href='https://3dfish-threejs.netlify.app'
+                    website='3dfish-threejs.netlify.app'
                 >
-                    react three fiber fish
+                    Implemented a 3D model in a website. Technologies i used include React, React-three-fiber and Three.js
+                </Project>
+                <Project
+                    imageSrc={wireframe}
+                    alt='wireframe threejs'
+                    title='Wireframe'
+                    href='https://wireframe-threejs.netlify.app'
+                    website='wireframe-threejs.netlify.app'
+                >
+                    A wireframe of a 3D torusknot. Learned in the Three.js course of Bruno Simon.
+                </Project>
+                <Project
+                    imageSrc={portfolio}
+                    alt='portfolio'
+                    title='Portfolio Website'
+                    href='https://www.thatguybryan.com/'
+                    website='thatguybryan.com'
+                >
+                    My personal portfolio website, which is still in development. Technologies i used to create this website include React, Three.js and Blender.
                 </Project>
                 <Project>
                     project2 text
--- src/components/content/helper/Project.jsx
+++ src/components/content/helper/Project.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 
-const Project = ({ imageSrc, alt = 'no text', children, title = 'default title' }) => {
+const Project = ({
+    imageSrc,
+    alt = 'no text',
+    children,
+    title = 'default title',
+    href = '#',
+    website = 'www.default.de'
+}
+) => {
     return (
         <div className="project">
             <img
                 className="project_image"
                 src={imageSrc}
                 alt={alt}
+                href={href}
+                website={website}
             />
             <p className="project_text">
-                <h4>{title}</h4>
+                <h4>{title}
+                    <a className='project_website' href={href}>{website}</a>
+                </h4>
                 {children}
             </p>
         </div>
